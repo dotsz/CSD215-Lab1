295de296987ec41a607fe17c049493cf7410fdf9
dotsz <23023987@saultcollege.ca>
2024-09-18 14:53:06 -0400

    test switch from school prof to personal prof

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -15,7 +15,6 @@ fun main() {
     // Load tasks from text file and make a list of Task objects
     loadTasksFromFile()
 
-    // test change
 
 
     // Main loop that will keep the app running until the user decides to quit
